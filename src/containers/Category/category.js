2d1d6596a130b092e2ee58200bd38b9d5876f20a
unknown <nguyenvanty10987654321000@gmail.com>
2021-03-01 23:53:01 +0700

    update 23232

--- src/containers/Category/category.js
+++ src/containers/Category/category.js
@@ -6,6 +6,12 @@ import { connect } from "react-redux";
 import * as actions from "./../../actions/index";
 import { Row, Col, Container, Button, Modal, Form } from "react-bootstrap";
 import SideBar from "../../components/SideBar/sidebar";
+import CheckboxTree from "react-checkbox-tree";
+import CheckBox from "@material-ui/icons/CheckBox";
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
+import KeyboardArrowDown from "@material-ui/icons/KeyboardArrowDown";
+import CheckBoxOutlineBlank from "@material-ui/icons/CheckBoxOutlineBlank";
+import "react-checkbox-tree/lib/react-checkbox-tree.css";
 function MyVerticallyCenteredModal(props) {
   return (
     <Modal
@@ -61,6 +67,25 @@ class category extends Component {
       options: [],
       data: [],
       image: "",
+      checked: [],
+      expanded: [],
+      expandedArray: [],
+      updateCategory: false,
+      temp : 3,
+      title : [
+        {
+          id : 0,
+          name : 'Add New Category'
+        },
+        {
+          id : 1,
+          name : 'Update Category'
+        },
+        {
+          id : 2,
+          name : 'Delete Category'
+        },
+      ]
     };
   }
   onChange = (e) => {
@@ -78,22 +103,43 @@ class category extends Component {
       name: this.state.name,
       image: this.state.image,
     };
-    console.log(data);
     this.props.onAddCategory(data);
     this.setState({
-      idParent : "",
-      name : "",
-      image : ""
+      idParent: "",
+      name: "",
+      image: "",
     });
   };
   setModalShow = () => {
     this.setState({
       modalShow: true,
+      temp : 0
     });
+  };
+  setModalShowCategory = () => {
+    this.setState({
+      modalShow: true,
+      temp : 1
+    });
+    console.log(this.state.checked);
+    console.log(this.state.expanded);
+  };
+  setModalShowCategoryDelete = () => {
+    this.setState({
+      modalShow: true,
+      temp : 2
+    });
+    console.log(this.state.checked);
+    console.log(this.state.expanded);
   };
   onHideShow = () => {
     this.setState({
       modalShow: false,
+    });
+  };
+  updateCategory = () => {
+    this.setState({
+      updateCategory: false,
     });
   };
   componentDidMount() {
@@ -108,11 +154,7 @@ class category extends Component {
         return (
           <li key={index}>
             {item.name}
-            {
-              <ul>
-                {this.getMenu(item.children)}
-              </ul>
-            }
+            {<ul>{this.getMenu(item.children)}</ul>}
           </li>
         );
       }
@@ -130,11 +172,9 @@ class category extends Component {
         this.getOptionMenu(item.children, options1);
       }
     });
-    console.log(options1);
     return options1;
   };
   createCategories = (categories, idParent = null) => {
-    console.log(categories);
     const categoryList = [];
     let category;
     if (idParent === null) {
@@ -144,16 +184,32 @@ class category extends Component {
     } else {
       category = categories.filter((cat) => cat.idParent === idParent);
     }
-    console.log(category);
     for (let cate of category) {
+      // categoryList.push({
+      //   id: cate.id,
+      //   name: cate.name,
+      //   idParent: cate.idParent,
+      //   children: this.createCategories(categories, cate.id),
+      // });
       categoryList.push({
-        id: cate.id,
-        name: cate.name,
+        value: cate.id,
+        label: cate.name,
         idParent: cate.idParent,
-        children: this.createCategories(categories, cate.id),
+        children: this.createCategories(categories, cate.id)
       });
     }
-    console.log(categoryList);
+    return categoryList;
+  };
+  createCategoriesTree = (categories) => {
+    const categoryList = [];
+    for (let cate of categories) {
+      categoryList.push({
+        value: cate.id,
+        label: cate.name,
+        idParent: cate.idParent,
+        children: cate.children && cate.children.length > 0 && this.createCategories(cate.children)
+      });
+    }
     return categoryList;
   };
   render() {
@@ -162,55 +218,66 @@ class category extends Component {
     var { category1 } = this.props;
     //var test = [...category];
     // lọc và hiển thị lại array sau khi nhận từ api
+    // sắp xếp json theo dạng cha-con
     var category = this.createCategories(category1);
     return (
       <Container fluid>
         <Row className="side_bar">
           <SideBar />
+
           <Col md={10} className="textAlign-justify side_bar-marginLeft">
             <Container>
               <Row>
                 <Col md={12}>
                   <div className="category">
-                    <h3>Category</h3>
+                    <h3>Add Category</h3>
                     <Button onClick={this.setModalShow} variant="success">
                       Add Category
                     </Button>
-
-                    <Modal
-                      show={this.state.modalShow}
-                      onHide={this.onHideShow}
-                      size="lg"
-                      aria-labelledby="contained-modal-title-vcenter"
-                      centered
-                    >
-                      <Modal.Header closeButton>
-                        <Modal.Title id="contained-modal-title-vcenter">
-                          Add New Category
-                        </Modal.Title>
-                      </Modal.Header>
-                      <Modal.Body>
-                        <Form onSubmit={this.onSubmit}>
-                          <Form.Group controlId="exampleForm.ControlInput1">
-                            <Form.Label>New category</Form.Label>
-                            <Form.Control
-                              type="text"
-                              name="name"
-                              value={this.state.name}
-                              onChange={this.onChange}
-                              placeholder="input category..."
-                            />
-                          </Form.Group>
-                          <Form.Group controlId="exampleForm.ControlSelect1">
-                            <Form.Label>Category select</Form.Label>
-                            <Form.Control
-                              name="idParent"
-                              onChange={this.onChange}
-                              value={this.state.idParent}
-                              as="select"
-                            >
-                              <option>category select</option>
-                              {this.getOptionMenu(category).map(
+                  </div>
+                  <Modal
+                    show={this.state.modalShow}
+                    onHide={this.onHideShow}
+                    size="lg"
+                    aria-labelledby="contained-modal-title-vcenter"
+                    centered
+                  >
+                    <Modal.Header closeButton>
+                      <Modal.Title id="contained-modal-title-vcenter">
+                        {
+                          this.state.title.map((values, index) => {
+                            if(values.id === this.state.temp) {
+                              return values.name;
+                            }
+                          })
+                          // this.state.temp === 0 ? <p>Add New Category</p> : 'Update Category'
+                        }
+                      </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                      <Form onSubmit={this.onSubmit}>
+                        <Form.Group controlId="exampleForm.ControlInput1">
+                          <Form.Label>New category</Form.Label>
+                          <Form.Control
+                            type="text"
+                            name="name"
+                            value={this.state.name}
+                            onChange={this.onChange}
+                            placeholder="input category..."
+                          />
+                        </Form.Group>
+                        <Form.Group controlId="exampleForm.ControlSelect1">
+                          <Form.Label>Category select</Form.Label>
+                          <Form.Control
+                            name="idParent"
+                            onChange={this.onChange}
+                            value={this.state.idParent}
+                            as="select"
+                          >
+                            <option>category select</option>
+                            {/* ép json dạng cha-con thẳng lại thành cây
+                              trong khi đó lại không dùng json nguyên mẫu rãnh thiệt */}
+                            {/* {this.getOptionMenu(category).map(
                                 (values, index) => {
                                   return (
                                     <option key={index} value={values.id}>
@@ -218,37 +285,175 @@ class category extends Component {
                                     </option>
                                   );
                                 }
-                              )}
-                            </Form.Control>
-                          </Form.Group>
-                          {/* <Form.Group> */}
-                          <Form.File
-                            type="file"
-                            name="image"
-                            onChange={this.onChange}
-                            id="exampleFormControlFile1"
-                            label="Picture category input"
-                          />
-                          {/* </Form.Group> */}
-                          <Modal.Footer>
-                            <Button
-                              onClick={this.onHideShow}
-                              type="submit"
-                              variant="primary"
-                            >
-                              Save Changes
-                            </Button>
-                            <Button onClick={this.onHideShow}>Close</Button>
-                          </Modal.Footer>
-                        </Form>
-                      </Modal.Body>
-                    </Modal>
-                  </div>
+                              )} */}
+                            {category1.map((values, index) => {
+                              return (
+                                <option key={index} value={values.id}>
+                                  {values.name}
+                                </option>
+                              );
+                            })}
+                          </Form.Control>
+                        </Form.Group>
+                        {/* <Form.Group> */}
+                        <Form.File
+                          type="file"
+                          name="image"
+                          onChange={this.onChange}
+                          id="exampleFormControlFile1"
+                          label="Picture category input"
+                        />
+                        {/* </Form.Group> */}
+                        <Modal.Footer>
+                          <Button
+                            onClick={this.onHideShow}
+                            type="submit"
+                            variant="primary"
+                          >
+                            Save Changes
+                          </Button>
+                          <Button onClick={this.onHideShow}>Close</Button>
+                        </Modal.Footer>
+                      </Form>
+                    </Modal.Body>
+                  </Modal>
+
+                  {/* Edit */}
+                  {/* <Modal
+                    show={this.state.updateCategory}
+                    onHide={this.updateCategory}
+                    size="lg"
+                    aria-labelledby="contained-modal-title-vcenter"
+                    centered
+                  >
+                    <Modal.Header closeButton>
+                      <Modal.Title id="contained-modal-title-vcenter">
+                        Update New Category
+                      </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                      <Form onSubmit={this.onSubmit}>
+                        <Row>
+                          <Col>
+                            <Form.Group controlId="exampleForm.ControlInput1">
+                              <Form.Label>New category</Form.Label>
+                              <Form.Control
+                                type="text"
+                                name="name"
+                                value={this.state.name}
+                                onChange={this.onChange}
+                                placeholder="input category..."
+                              />
+                            </Form.Group>
+                          </Col>
+
+                          <Col>
+                            <Form.Group controlId="exampleForm.ControlSelect1">
+                              <Form.Label>Category select</Form.Label>
+                              <Form.Control
+                                name="idParent"
+                                onChange={this.onChange}
+                                value={this.state.idParent}
+                                as="select"
+                              >
+                                <option>category select</option>
+                                {category1.map((values, index) => {
+                                  return (
+                                    <option key={index} value={values.id}>
+                                      {values.name}
+                                    </option>
+                                  );
+                                })}
+                              </Form.Control>
+                            </Form.Group>
+                          </Col>
+
+                          <Col>
+                            <Form.Group controlId="exampleForm.ControlSelect1">
+                              <Form.Label>Type select</Form.Label>
+                              <Form.Control
+                                name="idParent"
+                                onChange={this.onChange}
+                                value={this.state.idParent}
+                                as="select"
+                              >
+                                <option value="">Type select</option>
+                                <option value="store">Store</option>
+                                <option value="product">Product</option>
+                                <option value="page">Page</option>
+                              </Form.Control>
+                            </Form.Group>
+                          </Col>
+                        </Row>
+                        <Form.File
+                          type="file"
+                          name="image"
+                          onChange={this.onChange}
+                          id="exampleFormControlFile1"
+                          label="Picture category input"
+                        />
+                        <Modal.Footer>
+                          <Button
+                            onClick={this.updateCategory}
+                            type="submit"
+                            variant="primary"
+                          >
+                            Save Changes
+                          </Button>
+                          <Button onClick={this.updateCategory}>Close</Button>
+                        </Modal.Footer>
+                      </Form>
+                    </Modal.Body>
+                  </Modal> */}
                 </Col>
               </Row>
               <Row>
                 <Col md={12}>
-                  <ul>{this.getMenu(category)}</ul>
+                  {/* đọc json dạng cha-con này hiển thị lên UI */}
+                  {/* <ul>{this.getMenu(category)}</ul> */}
+                  <CheckboxTree
+                    nodes={this.createCategories(category1)}
+                    checked={this.state.checked}
+                    expanded={this.state.expanded}
+                    onCheck={(checked) => this.setState({ checked })}
+                    onExpand={(expanded) => this.setState({ expanded })}
+                    icons={{
+                      check: <CheckBox />,
+                      uncheck: <CheckBoxOutlineBlank />,
+                      halfCheck: <CheckBoxOutlineBlank />,
+                      expandClose: <KeyboardArrowRight />,
+                      expandOpen: <KeyboardArrowDown />,
+                    }}
+                  />
+                </Col>
+              </Row>
+              <Row>
+                <Col className="edit_category">
+                  <h3>Edit Category</h3>
+                  <Button onClick={this.setModalShowCategory} variant="primary">
+                    Update Category
+                  </Button>
+                  <Button onClick={this.setModalShowCategoryDelete} variant="danger">
+                    Delete Category
+                  </Button>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <CheckboxTree
+                    nodes={this.createCategoriesTree(category1)}
+                    checked={this.state.checked}
+                    expanded={this.state.expanded}
+                    onCheck={(checked) => this.setState({ checked })}
+                    onExpand={(expanded) => this.setState({ expanded })}
+                    icons={{
+                      check: <CheckBox />,
+                      uncheck: <CheckBoxOutlineBlank />,
+                      halfCheck: <CheckBoxOutlineBlank />,
+                      expandClose: <KeyboardArrowRight />,
+                      expandOpen: <KeyboardArrowDown />,
+                    }}
+                  />
                 </Col>
               </Row>
             </Container>
